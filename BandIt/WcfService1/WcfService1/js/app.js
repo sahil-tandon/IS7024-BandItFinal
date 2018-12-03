bba19a7f869462b395a1f87b7f2891679b1bac0b
kurawahn <kurawahn@mail.uc.edu>
2018-12-03 16:52:26 -0500

    Added images and changed target to new tab in band's website

--- BandIt/WcfService1/WcfService1/js/app.js
+++ BandIt/WcfService1/WcfService1/js/app.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
 				                    "<p class='band-origin'><span class='band-label'>Origin: </span>"+ bands.band[i].Origin +"</p>"+            
 				                    "<p class='band-founded'><span class='band-label'>Founded: </span>"+ bands.band[i].Datefounded +"</p>"+				                    
 				                    "<p class='band-members'><span class='band-label'>Members: </span>"+ bands.band[i].Members +"</p>"+                      
-                                    "<p class='band-site'><span class='band-label'>Website: </span><a href=//" + bands.band[i].Website + ">" + bands.band[i].Website + "</a></p>" +
+                                    "<p class='band-site'><span class='band-label'>Website: </span><a href=//" + bands.band[i].Website + " target='_blank'>" + bands.band[i].Website + "</a></p>" +
                                     "<p class='band-songs'><a href=SongListing.aspx?searchString=" + bands.band[i].BandName + ">View Songs</a></p>" +
 				                "</div>"+
 				            "</div>";
