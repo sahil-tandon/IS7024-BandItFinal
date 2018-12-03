87728b1d7b5e22b3327bd61820af49ef4d7e43b8
Sahil Tandon <tandonsl@mail.uc.edu>
2018-12-03 12:05:50 -0500

    BandIt | Restricting band population to Band Listing Page.

--- BandIt/WcfService1/WcfService1/js/app.js
+++ BandIt/WcfService1/WcfService1/js/app.js
@@ -9,20 +9,5 @@ $(document).ready(function(){
         }
     });
 
-    /*Populates bands using data from JSON*/
-    for(i=0;i<bands.band.length;i++){
-    	var bandTile = "<div class='band-tile'>"+
-    						"<img src='images/bands/acdc.jpg' class='band-image'>"+
-    						"<div class='band-info'>"+
-    							"<p class='name'>"+bands.band[i].BandName+"</p>"+
-    							"<p class='band-genre'><span class='band-label'>Genre: </span>"+bands.band[i].Genre+"</p>"+
-				                "<p class='band-origin'><span class='band-label'>Origin: </span>"+bands.band[i].Origin+"</p>"+            
-				                "<p class='band-founded'><span class='band-label'>Founded: </span>"+bands.band[i].Datefounded+"</p>"+
-				                "<p class='band-manager'><span class='band-label'>Manager: </span>"+bands.band[i].ManagerName+"</p>"+
-				                "<p class='band-members'><span class='band-label'>Members: </span>"+bands.band[i].Members+"</p>"+
-				                "<p class='band-site'><span class='band-label'>Website: </span><a href="+bands.band[i].Website+">"+bands.band[i].Website+"</a></p>"+
-				            "</div>"+
-				        "</div>";
-		$('.band-tile-wrap').append(bandTile);
     }
 });
