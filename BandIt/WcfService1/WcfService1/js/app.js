3e1a0f875021e412d94ebf3d760961190cd6d8fd
sudhakarsamak <sudhakarsamak@users.noreply.github.com>
2018-12-03 16:32:28 -0500

    View Songs functionality added

--- BandIt/WcfService1/WcfService1/js/app.js
+++ BandIt/WcfService1/WcfService1/js/app.js
@@ -19,8 +19,9 @@ $(document).ready(function(){
     							    "<p class='band-genre'><span class='band-label'>Genre: </span>"+ bands.band[i].Genre +"</p>"+
 				                    "<p class='band-origin'><span class='band-label'>Origin: </span>"+ bands.band[i].Origin +"</p>"+            
 				                    "<p class='band-founded'><span class='band-label'>Founded: </span>"+ bands.band[i].Datefounded +"</p>"+				                    
-				                    "<p class='band-members'><span class='band-label'>Members: </span>"+ bands.band[i].Members +"</p>"+
-				                    "<p class='band-site'><span class='band-label'>Website: </span><a href=//"+ bands.band[i].Website +">"+ bands.band[i].Website +"</a></p>"+
+				                    "<p class='band-members'><span class='band-label'>Members: </span>"+ bands.band[i].Members +"</p>"+                      
+                                    "<p class='band-site'><span class='band-label'>Website: </span><a href=//" + bands.band[i].Website + ">" + bands.band[i].Website + "</a></p>" +
+                                    "<p class='band-songs'><a href=SongListing.aspx?searchString=" + bands.band[i].BandName + ">View Songs</a></p>" +
 				                "</div>"+
 				            "</div>";
 		    $('.band-tile-wrap').append(bandTile);
