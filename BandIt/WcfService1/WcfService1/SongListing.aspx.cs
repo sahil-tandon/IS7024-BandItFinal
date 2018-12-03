0162952606ae0b6a37421a05376a4beeebe863fc
sudhakarsamak <sudhakarsamak@users.noreply.github.com>
2018-12-03 01:50:40 -0500

    SongListing Page created

---
+++ BandIt/WcfService1/WcfService1/SongListing.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WcfService1
+{
+    public partial class SongListing : System.Web.UI.Page
+    {
+        public String songJson = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String searchString = Request.QueryString["searchString"];
+            if (!searchString.Equals("") && searchString != null)
+                songJson = "";
+            else
+                songJson = JSONGenerator.generateBandJSON();
+        }
+    }
+}
